a4e8396719160de07543e3f26db98f798c7211d2
ashkan <ashkan.ram70@gmail.com>
2019-10-22 20:58:53 -0700

    Updates

---
+++ precache-manifest.55b1d857776f64eccf92beb28eaf0b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e3ec41c08b4df66dc782753fad30e0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d50aff277e2663554803",
+    "url": "/clicky-game/static/css/main.cd8d8adf.chunk.css"
+  },
+  {
+    "revision": "9501f22ec6282b72990a",
+    "url": "/clicky-game/static/js/2.99673906.chunk.js"
+  },
+  {
+    "revision": "d50aff277e2663554803",
+    "url": "/clicky-game/static/js/main.17014ba3.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
